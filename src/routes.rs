a7c22b4b4a0ef408265229ee4dad61f9d3792cfc
hayley-d <hayleydod@proton.me>
2025-01-03 19:37:01 +0200

    improve logging

--- src/routes.rs
+++ src/routes.rs
@@ -93,7 +93,8 @@ pub async fn create_document(
 
     // SQL query to insert a new snapshot into the document_snapshots table
     let snapshot_query = client.prepare("INSERT INTO document_snapshots (document_id,ssn,sum,sid,seq,value,tombstone) VALUES ($1,$2,$3,$4,$5,$6,$7)").await.map_err(|_| {
-        error!("Failed
+        error!("Failed to create INSERT query for document_snapshot table");
+        ApiError::DatabaseError(format!("Failed to create INSERT query for document_snapshot table"))
     })?;
     // SQL query to insert a new operation into the operations table
     let operation_query = r#"INSERT INTO operations (document_id,ssn,sum,sid,seq,value,tombstone,timestamp) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)"#;
