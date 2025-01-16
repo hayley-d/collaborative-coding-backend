27a09af6a2bfabb909f47c2ad7cc6e181e388ec1
hayley-d <hayleydod@proton.me>
2025-01-16 17:50:16 +0200

    clippy changes

--- replica/src/json_structures.rs
+++ replica/src/json_structures.rs
@@ -43,6 +43,8 @@ pub struct DocumentSnapshot {
 /// `value`: The value being Inserted/Updated (None if a delete operation)
 /// `s4vector`: The s4vector for the operation
 /// `tombstone`: Represents if the operation is logically deleted.
+/// `left`: The left s4vector of the operation (if it exists).
+/// `right`: The right s4vector of the opertion (if it exists)
 #[derive(Debug, Serialize, Deserialize)]
 pub struct OperationRequest {
     pub value: Option<String>,
