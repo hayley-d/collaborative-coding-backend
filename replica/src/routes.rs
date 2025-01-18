83d08e963ad0807837c5360aaa189bc2aeeae39e
hayley-d <hayleydod@proton.me>
2025-01-18 02:37:09 +0200

    clippy changes

--- replica/src/routes.rs
+++ replica/src/routes.rs
@@ -641,7 +641,10 @@ pub async fn delete(
         .await
     {
         Ok(obj) => obj,
-        Err(_) => return Err(ApiError::RequestFailed(format!("Error updating file"))),
+        Err(_) => {
+            error!(target:"error_logger","Failed to update file");
+            return Err(ApiError::RequestFailed("Error updating file".to_string()));
+        }
     };
 
     op.document_id = document_id;
