f5c1f23c9d7d6966b1fe1df7cb67e290260beff5
hayley-d <hayleydod@proton.me>
2025-01-24 16:55:43 +0200

    impl main function

--- load_balancer/src/main.rs
+++ load_balancer/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let mut nodes: Vec<String> = get_nodes();
+
+    // Listen on port 3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let listener: TcpListener = match TcpListener::bind(addr).await {
+        Ok(l) => l,
+        Err(_) => {
+            eprintln!("Failed to bind to socket");
+            std::process::exit(1);
+        }
+    };
+
+    println!("Listening on http://{}", addr);
+
+    let state: Arc<Mutex<LoadBalancer>> = Arc::new(Mutex::new(LoadBalancer::new(&mut nodes).await));
+
+    let shutdown: Arc<Notify> = Arc::new(Notify::new());
+
+    let shutdown_signal = shutdown.clone();
+
+    tokio::spawn(async move {
+        if let Err(_) = tokio::signal::ctrl_c().await {
+            eprintln!("Failed to listen for shutdown signal");
+            std::process::exit(1);
+        } else {
+            shutdown_signal.notify_one();
+            println!("Tasks complete, server shutdown started");
+            std::process::exit(0);
+        }
+    });
+
+    tokio::select! {
+        _ = reverse_proxy(listener,state.clone()) => {
+            println!("loop ended");
+        },
+        _ = shutdown.notified() => {
+                eprintln!("Graceful shutdown initiated");
+                std::process::exit(0);
+            }
+    }
+
+    Ok(())
 }
