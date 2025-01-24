f373b7bec5b1c86dd524496807415f8ab85b1e75
hayley-d <hayleydod@proton.me>
2025-01-24 16:26:06 +0200

    add Node struct

--- load_balancer/src/load_balancer.rs
+++ load_balancer/src/load_balancer.rs
@@ -11,4 +11,11 @@ pub mod consistent_hashing {
     use tonic::transport::Channel;
 
     const RATELIMITERADDRESS: &str = "http://127.0.0.1:50051";
+
+    /// Node represents a replica in the distributed system.
+    /// `address` is a url address for the replica
+    #[derive(Debug, PartialEq, Eq, Clone)]
+    pub struct Node {
+        pub address: String,
+    }
 }
